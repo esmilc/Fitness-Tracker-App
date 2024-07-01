47f7c16d26ea988f47230528a91ddfe3ccd724ab
esmilc <148603535+esmilc@users.noreply.github.com>
2024-06-30 20:20:34 -0400

    Modified the Welcome function to display a message

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from cli import cli
 
 unsaved_workouts = []
 
-# def welcome():
-#     print('Welcome to my Fitness Tracker App!\n\n')
-#     user_input = input("Would you like to start up the app? (y/n)  ")
-#     if user_input == 'y':
-#         return True
-#     else:
-#         return False
+def welcome():
+    print('-----------------------------------')
+    print('Welcome to my Fitness Tracker App!')
+    print('-----------------------------------\n')
+
 
 def main():
     init_db() #Initializing the database, where everything will be stored in
     cli() #This is what runs the command line tool
 
 if __name__ == "__main__":
+    welcome()
     main()
